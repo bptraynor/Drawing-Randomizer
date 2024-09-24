568126e07bc7b2ce3d82ca23336aa2b2cb6431c2
nachotraynor@gmail.com <nachotraynorart@gmail.com>
2024-09-24 17:18:23 -0400

    second commit

---
+++ Mac/drawing.app/Contents/Resources/app/drawing/index.js
@@ -0,0 +1,16 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+app.on('window-all-closed', () => {
+ app.quit()
+})
+
+
+app.on('ready', function() {
+  var mainWindow = new BrowserWindow({
+    width: 1600,
+    height: 1000,
+    
+  })
+  mainWindow.loadURL('file://' + __dirname + '/Test.html')
+})
